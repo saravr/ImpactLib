b961fcfdd96db8471a575e7476cd7dbe104bea59
sandymist <95550935+sandymist@users.noreply.github.com>
2021-12-28 14:30:38 -0800

    Rename package

--- impactlib/src/main/java/com/sensorapp/impactlib/ImpactMotionClassifier.kt
+++ impactlib/src/main/java/com/sensorapp/impactlib/ImpactMotionClassifier.kt
@@ -1,4 +1,4 @@
-package com.sandymist.impactlib
+package com.sensorapp.impactlib
 
 import android.content.Context
 import android.hardware.Sensor
--- impactlib/src/main/java/com/sensorapp/impactlib/PermissionActivity.kt
+++ impactlib/src/main/java/com/sensorapp/impactlib/PermissionActivity.kt
@@ -1,4 +1,4 @@
-package com.sandymist.impactlib
+package com.sensorapp.impactlib
 
 import android.Manifest
 import android.content.pm.PackageManager
