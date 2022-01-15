6d25ba13cd633d16b8cfc02261fb3b6ff8288a40
sandymist <95550935+sandymist@users.noreply.github.com>
2022-01-15 09:57:29 -0800

    Add amplitude

--- impactlib/src/main/java/com/sensorapp/impactlib/ImpactAudioClassifier.kt
+++ impactlib/src/main/java/com/sensorapp/impactlib/ImpactAudioClassifier.kt
@@ -2,16 +2,20 @@ package com.sensorapp.impactlib
 
 import android.content.Context
 import android.media.AudioRecord
+import android.media.MediaRecorder
 import android.os.Build
+import android.os.Environment
 import android.os.Handler
 import android.os.HandlerThread
+import android.os.Looper
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.os.HandlerCompat
 import org.tensorflow.lite.task.audio.classifier.AudioClassifier
 
 class ImpactAudioClassifier(private val context: Context, private val notifier: ImpactAudioNotifier) {
-
+    private var mediaRecorder = MediaRecorder()
+    private var mediaRecorderStarted = false
     private var audioClassifier: AudioClassifier? = null
     private var audioRecord: AudioRecord? = null
     private var classificationInterval = 500L // how often should classification run in milli-secs
@@ -32,6 +36,21 @@ class ImpactAudioClassifier(private val context: Context, private val notifier: 
         // If the audio classifier is initialized and running, do nothing.
         if (audioClassifier != null) return
 
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        /* Set the audio file encoding: AAC/AMR_NB/AMR_MB/Default Sampling (waveform) of the sound */
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT)
+        val outputFile = context.filesDir.absolutePath + "/test.3gp"
+        mediaRecorder.setOutputFile(outputFile)
+        /*
+         * Set the output file format: THREE_GPP/MPEG-4/RAW_AMR/Default THREE_GPP (3gp format,
+         * H263 video/ARM audio encoding), MPEG-4, RAW_AMR (only supports audio and audio encoding requires AMR_NB)
+         */
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+        //mMediaRecorder.setMaxDuration(MAX_LENGTH)
+        mediaRecorder.prepare()
+        mediaRecorder.start()
+
         // Initialize the audio classifier
         val classifier = AudioClassifier.createFromFile(context, MODEL_FILE)
         val audioTensor = classifier.createInputTensorAudio()
@@ -45,6 +64,8 @@ class ImpactAudioClassifier(private val context: Context, private val notifier: 
             @RequiresApi(Build.VERSION_CODES.M)
             override fun run() {
                 //val startTime = System.currentTimeMillis()
+
+                //Log.e(TAG, "+++++ AMP: " + mediaRecorder.maxAmplitude)
 
                 // Load the latest audio sample
                 audioTensor.load(record)
@@ -64,7 +85,7 @@ class ImpactAudioClassifier(private val context: Context, private val notifier: 
                 val audioTypes = filteredModelOutput.joinToString {
                     it.label
                 }
-                notifier.reportAudioEvent(audioTypes)
+                notifier.reportAudioEvent(audioTypes, mediaRecorder.maxAmplitude)
 
                 // Rerun the classification after a certain interval
                 handler.postDelayed(this, classificationInterval)
@@ -97,5 +118,5 @@ class ImpactAudioClassifier(private val context: Context, private val notifier: 
 }
 
 interface ImpactAudioNotifier {
-    fun reportAudioEvent(audioTypes: String)
+    fun reportAudioEvent(audioTypes: String, maxAmplitude: Int)
 }
