5795e865c535c76cc500ba79eb5ae0c94f39bd89
sandymist <95550935+sandymist@users.noreply.github.com>
2022-02-05 22:05:23 -0800

    Add pref prog.

--- impactlib/src/main/java/com/sensorapp/impactlib/SettingsFragment.kt
+++ impactlib/src/main/java/com/sensorapp/impactlib/SettingsFragment.kt
@@ -1,10 +1,25 @@
 package com.sensorapp.impactlib
 
 import android.os.Bundle
+import android.text.InputType
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment: PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        context?.let {
+            val pref = EditTextPreference(it)
+            pref.key = "age"
+            pref.title = "Age"
+            pref.setDefaultValue("52")
+            pref.summaryProvider = EditTextPreference.SimpleSummaryProvider.getInstance()
+            pref.setOnBindEditTextListener { editText ->
+                editText.inputType = InputType.TYPE_CLASS_NUMBER
+            }
+
+            preferenceScreen.addPreference(pref)
+        }
     }
 }
